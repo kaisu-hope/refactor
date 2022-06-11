0beb1a70d485eed01e9eec143cef23f82dfe5a52
kaisu <ks2997854254@gmail.com>
2022-06-11 18:29:11 +0800

    feat: feign project model construct

---
+++ feign/start/src/main/java/com/kaisu/feign/FeignBootstrap.java
@@ -0,0 +1,14 @@
+package com.kaisu.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kaisu
+ */
+@SpringBootApplication
+public class FeignBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignBootstrap.class, args);
+    }
+}
